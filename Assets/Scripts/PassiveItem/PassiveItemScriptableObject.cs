b8891708b8902bec8f4e099d18a0ac51080c4b5a
khanh duy <khanhduy25072001@gmail.com>
2023-04-07 09:57:53 +0700

    Chinh lai inventory

--- Assets/Scripts/PassiveItem/PassiveItemScriptableObject.cs
+++ Assets/Scripts/PassiveItem/PassiveItemScriptableObject.cs
@@ -8,15 +8,14 @@ public class PassiveItemScriptableObject : ScriptableObject
     [SerializeField]
     float multipler;
     public float Multipler { get => multipler; private set => multipler = value; }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    int level;
+    public int Level {get => level; private set => level = value;}
+    [SerializeField]
+    GameObject nextLevelPrefab;
+    public GameObject NextLevelPrefab {get => nextLevelPrefab; private set => nextLevelPrefab = value;}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    Sprite icon;
+    public Sprite Icon {get => icon; private set => icon = value;}
 }
--- Assets/Scripts/Scriptable Objects/Weapons/WeaponScriptableObject.cs
+++ Assets/Scripts/Scriptable Objects/Weapons/WeaponScriptableObject.cs
@@ -29,4 +29,14 @@ public class WeaponScriptableObject : ScriptableObject
     // xuyen dich
     int pierce;
     public int Pierce { get => pierce; private set => pierce = value; }
+    [SerializeField]
+    int level;
+    public int Level {get => level; private set => level = value;}
+    [SerializeField]
+    GameObject nextLevelPrefab;
+    public GameObject NextLevelPrefab {get => nextLevelPrefab; private set => nextLevelPrefab = value;}
+
+    [SerializeField]
+    Sprite icon;
+    public Sprite Icon {get => icon; private set => icon = value;}
 }
